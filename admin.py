b13a38efd65c7ef9e657fb3765cf415f3be2411c
sonsarigo07-pixel <sonsarigo07@gmail.com>
2026-09-26 01:17:50 +0300

    clear

--- admin.py
+++
@@ -1,5 +0,0 @@
-
-from django.contrib import admin
-from .models import Patient, Bill
-admin.site.register(Patient)
-admin.site.register(Bill)
--- serializers.py
+++
@@ -1,7 +0,0 @@
-
-from rest_framework import serializers
-from .models import Bill, Patient
-class BillSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Bill
-        fields = '__all__'
